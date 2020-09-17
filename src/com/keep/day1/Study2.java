15c12a7abc01f8ead03a5260bdec0997e75219b3
txcHello <txc.123456>
2020-09-18 00:21:38 +0800

    hello world say

--- src/com/keep/day1/Study2.java
+++ src/com/keep/day1/Study2.java
@@ -3,5 +3,11 @@ package com.keep.day1;
 public class Study2 {
     public static void main(String[] args) {
         System.out.println("hello world");
+        Study2.sayHello();
+    }
+
+
+    public static void sayHello() {
+        System.out.println("hello");
     }
 }
